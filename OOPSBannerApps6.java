ea9f657bcb1e95995b27d387a0767b96025fe91a
S-A-Sarvajit <ss8139@srmist.edu.in>
2026-03-03 10:54:02 +0530

    OOPSBannerApp5.java added

---
+++ OOPSBannerApps6.java
@@ -0,0 +1,18 @@
+public class OOPSBannerApps6 {
+    public static string[] getOpattern(){
+        
+    }
+
+    
+    public static string[] getPpattern(){
+        System.out.println("     ***     ");
+        System.out.println("    ** **    ");
+        System.out.println("   **   **   ");
+        System.out.println("   **   **   ");
+        System.out.println("   **   **   ");
+        System.out.println("    ** **    ");
+        System.out.println("     ***     ");
+
+    }
+    
+}
